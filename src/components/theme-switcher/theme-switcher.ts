3cca1bf300fa152dad45ee8f42f51810f4240daf
xiaojamie <51051637+xiaojamie@users.noreply.github.com>
2024-04-26 22:12:36 +0800

    Update theme-switcher.ts

--- src/components/theme-switcher/theme-switcher.ts
+++ src/components/theme-switcher/theme-switcher.ts
@@ -110,19 +110,19 @@ export class ThemeSwitcher extends LitElement {
 
     const _heroImage = document.querySelector('#home-hero-image') as HTMLImageElement;
 		if (theme === 'default') {
-			_heroImage.src = '/assets/images/home/classic-hero.jpg';
+			_heroImage.src = '/webjamie.github.io/assets/images/home/classic-hero.jpg';
 		}
 		if (theme === 'dark') {
-			_heroImage.src = '/assets/images/home/dark-hero.jpg';
+			_heroImage.src = '/webjamie.github.io/assets/images/home/dark-hero.jpg';
 		}
 		if (theme === 'earth') {
-			_heroImage.src = '/assets/images/home/earth-hero.jpg';
+			_heroImage.src = '/webjamie.github.io/assets/images/home/earth-hero.jpg';
 		}
 		if (theme === 'ocean') {
-			_heroImage.src = '/assets/images/home/ocean-hero.jpg';
+			_heroImage.src = '/webjamie.github.io/assets/images/home/ocean-hero.jpg';
 		}
 		if (theme === 'sand') {
-			_heroImage.src = '/assets/images/home/sand-hero.jpg';
+			_heroImage.src = '/webjamie.github.io/assets/images/home/sand-hero.jpg';
 		}
 		localStorage.setItem('theme', theme);
 		this.theme = theme;
